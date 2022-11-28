85503ff1ea0e4eb664f044e191554a99de715c1b
fasilmsageer <fasilmsageer@gmail.com>
2022-11-28 08:53:00 +0530

    bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AddFriendComponent } from './add-friend/add-friend.component';
 import { ViewAllComponent } from './view-all/view-all.component';
 import { RouterComponent } from './router/router.component';
 import { RouterModule } from '@angular/router';
+import { SearchfriendComponent } from './searchfriend/searchfriend.component';
 
 const myRoute=[
   {
@@ -24,7 +25,8 @@ const myRoute=[
     AppComponent,
     AddFriendComponent,
     ViewAllComponent,
-    RouterComponent
+    RouterComponent,
+    SearchfriendComponent
   ],
   imports: [
     BrowserModule,
